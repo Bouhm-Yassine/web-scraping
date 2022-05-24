3c0a3d82b8b06bd0e2d3598ae788ae155fe1dffa
BOUHM Yassine <yassine.bouhm@edu.uiz.ac.ma>
2022-05-24 13:55:04 +0100

    add scraping uk

--- web-scraping.py
+++ web-scraping.py
@@ -1,11 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = "https://www.societe.com/cgi-bin/search?champs=cartan"
+url_1 = "https://www.societe.com/cgi-bin/search?champs=cartan"
+url_2 = "https://find-and-update.company-information.service.gov.uk/search/companies?q=cartan"
 
 
-def scrap():
-    response = requests.get(url, headers={'User-agent': 'Super Bot Power Level Over 9000'})
+def scrap_1():
+    response = requests.get(url_1, headers={'User-agent': 'Super Bot Power Level Over 9000'})
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'lxml')
         result_search = soup.find('div', id='search')
@@ -35,5 +36,37 @@ def scrap():
         return 'THROW EXCPETION HERE'
 
 
+def scrap_2():
+    response = requests.get(url_2, headers={'User-agent': 'Super Bot Power Level Over 9000'})
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'lxml')
+        result_search = soup.find('article', id="services-information-results")
+
+        if result_search is None:
+            return 'Aucun résultat trouvé'
+
+        result = soup.find('ul', id='results')
+        companies = result.findChildren("li", recursive=False)
+        for company in companies[:3]:
+            name = company.a.text.strip()
+
+            national_date = company.find('p', class_='meta').text
+            registred_as = national_date.split('-')[0].strip()
+
+            full_date = national_date.split('-')[-1]
+            date = full_date.replace('Incorporated on', '').replace('Dissolved on', '').strip()
+
+            adress_line = company.find_all('p')[-1].text.strip()
+            postal_code = adress_line.split(',')[-1].strip()
+            json = {"name": name, "status": "active", "registred_as": registred_as, 'created_at': date,
+                    "adress_line": adress_line, "postal_code": postal_code}
+            print(json)
+            print('===============================')
+
+    else:
+        return 'THROW EXCPETION HERE'
+
+
 if __name__ == '__main__':
-    scrap()
+    # scrap_1()
+    scrap_2()
